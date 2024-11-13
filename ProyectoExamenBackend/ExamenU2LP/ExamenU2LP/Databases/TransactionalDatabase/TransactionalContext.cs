eea4c6d6d81ad69b99ea1a43fdf89f4d2256bc1a
anyta58 <henriquezvictoria58@gmail.com>
2024-11-13 12:59:57 -0600

    Quitando comentarios context

--- ProyectoExamenBackend/ExamenU2LP/ExamenU2LP/Databases/TransactionalDatabase/TransactionalContext.cs
+++ ProyectoExamenBackend/ExamenU2LP/ExamenU2LP/Databases/TransactionalDatabase/TransactionalContext.cs
@@ -49,36 +49,36 @@ public class TransactionalContext : IdentityDbContext<UserEntity>
         }
     }
 
-    //public override Task<int> SaveChangesAsync
-    //    (
-    //    CancellationToken cancellationToken = default
-    //    )
-    //{
-    //    var entries = ChangeTracker
-    //        .Entries()
-    //        .Where(e => e.Entity is BaseEntity && (
-    //            e.State == EntityState.Added ||
-    //            e.State == EntityState.Modified
-    //        ));
-    //    foreach (var entry in entries)
-    //    {
-    //        var entity = entry.Entity as BaseEntity;
-    //        if (entity != null)
-    //        {
-    //            if (entry.State == EntityState.Added)
-    //            {
-    //                entity.CreatedBy = _auditService.GetUserId();
-    //                entity.CreatedDate = DateTime.Now;
-    //            }
-    //            else
-    //            {
-    //                entity.UpdatedBy = _auditService.GetUserId();
-    //                entity.UpdatedDate = DateTime.Now;
-    //            }
-    //        }
-    //    }
-    //    return base.SaveChangesAsync(cancellationToken);
-    //}
+    public override Task<int> SaveChangesAsync
+        (
+        CancellationToken cancellationToken = default
+        )
+    {
+        var entries = ChangeTracker
+            .Entries()
+            .Where(e => e.Entity is BaseEntity && (
+                e.State == EntityState.Added ||
+                e.State == EntityState.Modified
+            ));
+        foreach (var entry in entries)
+        {
+            var entity = entry.Entity as BaseEntity;
+            if (entity != null)
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entity.CreatedBy = _auditService.GetUserId();
+                    entity.CreatedDate = DateTime.Now;
+                }
+                else
+                {
+                    entity.UpdatedBy = _auditService.GetUserId();
+                    entity.UpdatedDate = DateTime.Now;
+                }
+            }
+        }
+        return base.SaveChangesAsync(cancellationToken);
+    }
 
     public DbSet<AccountBalanceEntity> AccountBalances { get; set; }
     public DbSet<AccountBehaviorTypeEntity> AccountBehaviorTypes { get; set; }
